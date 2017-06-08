0ec880ae42bbd96ecd44d40e7337e1648db5b031
Muhammad Sayuthi <sayuthi@rbtsb.com>
2017-06-08 12:34:21 +0800

    add filter pipe

--- demo/demo.module.ts
+++ demo/demo.module.ts
@@ -4,9 +4,10 @@ import { MsmsUtilsModule } from "../src";
 import { DemoComponent } from "./demo.component";
 import { DemoOrderByComponent } from "./pipes/demo-order-by.component";
 import { DemoMappedByComponent } from "./pipes/demo-mapped-by.component";
+import { DemoFilterByComponent } from "./pipes/demo-filter-by.component";
 
 @NgModule({
-  declarations: [DemoComponent, DemoOrderByComponent, DemoMappedByComponent],
+  declarations: [DemoComponent, DemoOrderByComponent, DemoMappedByComponent, DemoFilterByComponent],
   imports: [BrowserModule, MsmsUtilsModule],
   bootstrap: [DemoComponent]
 })
--- src/msms-utils.module.ts
+++ src/msms-utils.module.ts
@@ -3,14 +3,13 @@ import { CommonModule } from "@angular/common";
 import { FormsModule } from "@angular/forms";
 import { OrderByPipe } from "./pipes/order-by.pipe";
 import { MapByPipe } from "./pipes/map-by.pipe";
-
+import { FilterByPipe } from "./pipes/filter-by.pipe";
 @NgModule({
   declarations: [
-    OrderByPipe,
-    MapByPipe
+    OrderByPipe, MapByPipe, FilterByPipe
   ],
   imports: [CommonModule, FormsModule],
-  exports: [OrderByPipe, MapByPipe]
+  exports: [OrderByPipe, MapByPipe, FilterByPipe]
 })
 export class MsmsUtilsModule {
   static forRoot(): ModuleWithProviders {
